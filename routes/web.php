a70c469a7d029d767816f2a15580b56a5e960b43
selinakkas <sselinakkas@gmail.com>
2024-01-24 18:29:47 +0300

    24.01

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,67 +29,29 @@ class Task
   }
 }
 
-$tasks = 
-[
-  new Task(
-    1,
-    'Buy groceries',
-    'Task 1 description',
-    'Task 1 long description',
-    false,
-    '2023-03-01 12:00:00',
-    '2023-03-01 12:00:00'
-  ),
-  new Task(
-    2,
-    'Sell old stuff',
-    'Task 2 description',
-    null,
-    false,
-    '2023-03-02 12:00:00',
-    '2023-03-02 12:00:00'
-  ),
-  new Task(
-    3,
-    'Learn programming',
-    'Task 3 description',
-    'Task 3 long description',
-    true,
-    '2023-03-03 12:00:00',
-    '2023-03-03 12:00:00'
-  ),
-  new Task(
-    4,
-    'Take dogs for a walk',
-    'Task 4 description',
-    null,
-    false,
-    '2023-03-04 12:00:00',
-    '2023-03-04 12:00:00'
-  ),
-];
-
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use ($tasks)
-{
+Route::get('/tasks', function () 
+{ 
     return view('index' , [
-        'tasks'=> $tasks
+        'tasks'=> \App\Models\Task::latest()->where('completed',false)->get(),
     ]); 
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-  #firstWhere fonksiyonu, belirli bir şarta uyan ilk elemanı döndürür. 
-    $task = collect($tasks)->firstWhere('id', $id);
-    #Eğer belirli bir id'ye sahip görev bulunamazsa, 'abort' fonksiyonu kullanılarak HTTP 404 Not Found hatası üretilir. Bu, kullanıcıya belirtilen id ile bir görevin bulunamadığını belirten bir hata sayfası gösterir.
-    if (!$task) {
-        abort(Response::HTTP_NOT_FOUND);
-    }
-    #Eğer belirli bir görev bulunursa, 'view' fonksiyonu ile 'show.blade.php' adlı görünüm dosyasını çağırır ve bu görünüme '$task' değişkenini aktarır. Bu, belirli bir görevin detaylarının görüntülendiği sayfayı oluşturur.
-    return view('show', ['task' => $task]); 
+Route::view('/tasks/create' , 'create')->name('tasks.create')
+  ->name('tasks.create');
+
+Route::get('/tasks/{id}', function ($id) {
+    return view('show', ['task' => \App\Models\Task::findOrFail( $id )]); 
 })->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+  dd($request->all());
+})->name('tasks.store');
+
+
 
 Route::fallback(function () {
     return 'still got somewhere!';
